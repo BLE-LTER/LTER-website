7b8786223834648e1018948a44e2b35692b47b5e
Tim Whiteaker <whiteaker@utexas.edu>
2018-04-04 12:26:52 -0500

    Add lazy loading and multiple image sizes

--- public/js/app.js
+++ public/js/app.js
@@ -12,3 +12,14 @@ function siteSearchKeypress(e) {
     launchSiteSearch();
   }
 }
+
+// Lazy load images, https://davidwalsh.name/lazyload-image-fade
+[].forEach.call(document.querySelectorAll('noscript'), function(noscript) {
+  if (noscript.getAttribute('data-src')) {
+    var img = new Image();
+    noscript.parentNode.insertBefore(img, noscript);
+    img.src = noscript.getAttribute('data-src');
+    img.className = noscript.getAttribute('img-class');
+    img.alt = noscript.getAttribute('alt');
+  }
+});
