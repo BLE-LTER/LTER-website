11f7e70976bfb3829328ef873f3e84b52f41acd4
Tim Whiteaker <whiteaker@utexas.edu>
2018-04-05 15:30:30 -0500

    add service worker

--- public/js/app.js
+++ public/js/app.js
@@ -23,3 +23,14 @@ function siteSearchKeypress(e) {
     img.alt = noscript.getAttribute('alt');
   }
 });
+
+// Install service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
