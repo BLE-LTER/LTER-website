f49def05fb223245511a65a9276f08a47f6690a3
Tim Whiteaker <whiteaker@utexas.edu>
2018-04-06 09:32:19 -0500

    Add items to service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,22 @@
 var CACHE_NAME = 'ble-cache-v1';
 var urlsToCache = [
-  '/'
+  '/',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+  'https://d33wubrfki0l68.cloudfront.net/css/37f58c2668ae65fbfe1132d9505859f6261a14ce/css/app.css',
+  'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+  'https://d33wubrfki0l68.cloudfront.net/js/158a44e0bc59abfaf9d7dac473ff0c4f44753a3a/js/app.js',
+  'https://use.fontawesome.com/a989b590e9.js',
+  'https://unpkg.com/lunr/lunr.js',
+  'https://d33wubrfki0l68.cloudfront.net/bundles/2639a19f1670d311559eb51171a47278f2730e9e.js',
+  '/index.html',
+  '/about.html',
+  '/people.html',
+  '/search.html'
 ];
 
 self.addEventListener('install', function(event) {
+  console.log("[Servicework] Install");
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
@@ -14,6 +27,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  console.log("[ServiceWorker] Fetch");
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
@@ -43,9 +57,8 @@ self.addEventListener('fetch', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-
+  console.log("[Servicework] Activate");
   var cacheWhitelist = [CACHE_NAME];
-
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
