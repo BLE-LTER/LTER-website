ac51a5a40448f1cd61255e9dd28923e2f6ecb226
Tim Whiteaker <whiteaker@utexas.edu>
2018-04-06 09:41:34 -0500

    Remove cors-unfriendly fontawesome from service worker cache

--- public/sw.js
+++ public/sw.js
@@ -6,7 +6,6 @@ var urlsToCache = [
   'https://code.jquery.com/jquery-3.2.1.slim.min.js',
   'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
   'https://d33wubrfki0l68.cloudfront.net/js/158a44e0bc59abfaf9d7dac473ff0c4f44753a3a/js/app.js',
-  'https://use.fontawesome.com/a989b590e9.js',
   'https://unpkg.com/lunr/lunr.js',
   'https://d33wubrfki0l68.cloudfront.net/bundles/2639a19f1670d311559eb51171a47278f2730e9e.js',
   '/index.html',
