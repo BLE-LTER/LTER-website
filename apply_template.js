335564b57f6d6c3f8d326380b7fceda381eab8a6
Tim Whiteaker <whiteaker@utexas.edu>
2018-10-09 16:10:14 -0500

    Switch to wide nav menu

--- apply_template.js
+++ apply_template.js
@@ -8,7 +8,7 @@ var cheerio = require("cheerio");
 // Change these constants to suit your needs
 const HTML_FOLDER = path.join(__dirname, 'public');;  // folder with your HTML files
 const EXCLUDE_FILES = ["google1b139a0c8d361fbe.html"];
-const TEMPLATE = "templates/bib_to_pub.html";  // template file
+const TEMPLATE = "templates/wide_menu.html";  // template file
 
 
 function isHtml(filename) {
