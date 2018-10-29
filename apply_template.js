6c47216e1be88340ef1fbee19a8f35113601c867
Tim Whiteaker <whiteaker@utexas.edu>
2018-10-29 15:58:20 -0500

    add Data to top level menu, for realsies

--- apply_template.js
+++ apply_template.js
@@ -8,7 +8,7 @@ var cheerio = require("cheerio");
 // Change these constants to suit your needs
 const HTML_FOLDER = path.join(__dirname, 'public');;  // folder with your HTML files
 const EXCLUDE_FILES = ["google1b139a0c8d361fbe.html"];
-const TEMPLATE = "templates/wide_menu.html";  // template file
+const TEMPLATE = "templates/add_data.html";  // template file
 
 
 function isHtml(filename) {
