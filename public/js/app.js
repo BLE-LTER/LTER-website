f55a879ae1748866494702c1301b08ce33b43e8c
Tim Whiteaker <whiteaker@utexas.edu>
2020-05-29 13:53:06 -0500

    remove service worker

--- public/js/app.js
+++ public/js/app.js
@@ -40,15 +40,15 @@ function getParameterByName(name, url) {
 
 
 // Install service worker
-if ('serviceWorker' in navigator) {
-   window.addEventListener('load', function () {
-      navigator.serviceWorker.register('/sw.js').then(function (registration) {
-         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function (err) {
-         console.log('ServiceWorker registration failed: ', err);
-      });
-   });
-}
+// if ('serviceWorker' in navigator) {
+//    window.addEventListener('load', function () {
+//       navigator.serviceWorker.register('/sw.js').then(function (registration) {
+//          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//       }, function (err) {
+//          console.log('ServiceWorker registration failed: ', err);
+//       });
+//    });
+// }
 
 
 function showLoading(isLoading) {
