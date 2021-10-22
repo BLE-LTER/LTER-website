3c2d05f162e1bdde851ea4b9ad035e5320c3bcd0
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2021-10-22 03:29:21 -0500

    first pass at revamped data pages

---
+++ public/js/collection-data.js
@@ -0,0 +1,162 @@
+var data = [
+    {
+        "DataSetID" : 1,
+        "Title" : "Carbon flux from aquatic ecosystems of the Arctic Coastal Plain along the Beaufort Sea, Alaska, 2010-2018",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=1",
+        "is_core" : false,
+        "temporal_type" : "short_term_study",
+        "theme" : "Biogeochemistry and nutrient cycling"
+    },
+    {
+        "DataSetID" : 2,
+        "Title" : "Dissolved organic carbon (DOC) and total dissolved nitrogen (TDN) from river, lagoon, and open ocean sites along the Alaska Beaufort Sea coast, 2018-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=2",
+        "is_core" : true,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Biogeochemistry and nutrient cycling"
+    },
+    {
+        "DataSetID" : 3,
+        "Title" : "Physiochemical water column parameters and hydrographic time series from river, lagoon, and open ocean sites along the Alaska Beaufort Sea coast, 2018-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=3",
+        "is_core" : true,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Lagoon and sea interactions"
+    },
+    {
+        "DataSetID" : 4,
+        "Title" : "Stable oxygen isotope ratios of water (H2O-d18O) from river, lagoon, and open ocean sites along the Alaska Beaufort Sea coast, 2019-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=4",
+        "is_core" : true,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Biogeochemistry and nutrient cycling"
+    },
+    {
+        "DataSetID" : 5,
+        "Title" : "Model simulated hydrological estimates for the North Slope drainage basin, Alaska, 1980-2010",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=5",
+        "is_core" : false,
+        "temporal_type" : "completed_timeseries",
+        "theme" : "Water and nutrient inputs"
+    },
+    {
+        "DataSetID" : 6,
+        "Title" : "Photosynthetically active radiation (PAR) time series from lagoon sites along the Alaska Beaufort Sea coast, 2018-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=6",
+        "is_core" : true,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Biological communities"
+    },
+    {
+        "DataSetID" : 7,
+        "Title" : "Circulation dynamics: currents, waves, temperature measurements from moorings in lagoon sites along the Alaska Beaufort Sea coast, 2018-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=7",
+        "is_core" : false,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Lagoon and sea interactions"
+    },
+    {
+        "DataSetID" : 8,
+        "Title" : "Seasonal Ice Mass-balance Buoy (SIMB) measurements from sites along the Beaufort Sea Coast, Alaska, 2018-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=8",
+        "is_core" : false,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Lagoon and sea interactions"
+    },
+    {
+        "DataSetID" : 9,
+        "Title" : "Time series of water column pH from lagoon sites along the Alaska Beaufort Sea coast, 2018-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=9",
+        "is_core" : true,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Biogeochemistry and nutrient cycling"
+    },
+    {
+        "DataSetID" : 10,
+        "Title" : "Catalog of GenBank sequence read archive (SRA) entries of 16S and 18S rRNA genes from bacterial and protistan planktonic communities along the Eastern Beaufort Sea coast, North Slope, Alaska, 2011-2013",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=10",
+        "is_core" : false,
+        "temporal_type" : "short_term_study",
+        "theme" : "Biological communities"
+    },
+    {
+        "DataSetID" : 11,
+        "Title" : "Carbon and nitrogen content and stable isotope compositions from particulate organic matter samples from lagoon, river, and open ocean sites along the Alaska Beaufort Sea coast, 2018-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=11",
+        "is_core" : true,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Biogeochemistry and nutrient cycling"
+    },
+    {
+        "DataSetID" : 12,
+        "Title" : "Sediment pigment concentrations from lagoon sites along the Alaska Beaufort Sea coast, 2018-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=12",
+        "is_core" : true,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Biological communities"
+    },
+    {
+        "DataSetID" : 13,
+        "Title" : "Water column chlorophyll concentrations from lagoon, river, and ocean sites along the Alaska Beaufort Sea coast, 2018-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=13",
+        "is_core" : true,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Biological communities"
+    },
+    {
+        "DataSetID" : 14,
+        "Title" : "Water column and sediment porewater nutrient concentrations from lagoon, river, and ocean sites along the Alaska Beaufort Sea coast, 2018-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=14",
+        "is_core" : true,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Biogeochemistry and nutrient cycling"
+    },
+    {
+        "DataSetID" : 15,
+        "Title" : "Colored dissolved organic matter (CDOM) absorbance from lagoon, ocean, and river sites along the Alaska Beaufort Sea coast, 2019-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=15",
+        "is_core" : true,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Biogeochemistry and nutrient cycling"
+    },
+    {
+        "DataSetID" : 16,
+        "Title" : "Flow direction grid at 1 kilometer resolution for North Slope drainage basins, Alaska",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=16",
+        "is_core" : false,
+        "temporal_type" : "non_temporal",
+        "theme" : "Water and nutrient inputs"
+    },
+    {
+        "DataSetID" : 17,
+        "Title" : "Geochemical characterization and material properties of coastal permafrost near Drew Point, Alaska",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=17",
+        "is_core" : false,
+        "temporal_type" : "short_term_study",
+        "theme" : "Water and nutrient inputs"
+    },
+    {
+        "DataSetID" : 18,
+        "Title" : "Carbon and nitrogen content and stable isotope composition from sediment organic matter from lagoon sites along the Alaska Beaufort Sea coast, 2018-ongoing",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=18",
+        "is_core" : true,
+        "temporal_type" : "ongoing_timeseries",
+        "theme" : "Biogeochemistry and nutrient cycling"
+    },
+    {
+        "DataSetID" : 19,
+        "Title" : "Catalog of GenBank sequence read archive (SRA) entries of metagenomic DNA sequence analyses of bacterial and archaeal water column communities along the Eastern Beaufort Sea coast, North Slope, Alaska, 2012",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=19",
+        "is_core" : false,
+        "temporal_type" : "short_term_study",
+        "theme" : "Biological communities"
+    },
+    {
+        "DataSetID" : 23,
+        "Title" : "Model estimates of runoff, dissolved organic carbon, soil temperature and moisture for Elson Lagoon watershed, Alaska, 1981-2020",
+        "url" : "https:\/\/portal.edirepository.org\/nis\/mapbrowse?scope=knb-lter-ble&identifier=23",
+        "is_core" : false,
+        "temporal_type" : "short_term_study",
+        "theme" : "Water and nutrient inputs"
+    }
+]
